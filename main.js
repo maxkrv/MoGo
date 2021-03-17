3d4dd703ef0c705441ef9c5cf08f94f90dc4f90e
Max Krivosheia <maxkr1203@gmail.com>
2021-03-17 11:38:36 +0200

    almost finished responsive design

--- main.js
+++ main.js
@@ -1,1 +1,45 @@
-let a = 1;
+$(function() {
+    let header = $("#header");
+    let intro = $("#intro").innerHeight();
+    let scrollPos = $(window).scrollTop();
+
+    // header
+    function checkScroll() {
+        if(scrollPos > intro) {
+            header.addClass("fixed")
+        } else {
+            header.removeClass("fixed");
+        }
+    }
+
+    checkScroll();
+
+    $(window).on("scroll resize", function() {
+        scrollPos = $(this).scrollTop();      
+        checkScroll();
+        $("#nav").removeClass("active");
+        $("#burger").removeClass("active")
+    });
+
+    // scroll
+    $("[data-scroll]").on("click", function(e) {
+        e.preventDefault();
+
+        let elementID = $(this).data("scroll");
+        let elementOffset = $(elementID).offset().top;
+
+        $("html, body").animate({
+            scrollTop: elementOffset - 50
+        }, 800);
+    });
+
+    // burger-menu
+    $("#burger").on("click", function(e) {
+        e.preventDefault();
+        
+        $(this).toggleClass("active");
+        $("#nav").toggleClass("active");
+    });
+
+    // accordion
+});
